a931098fe1eaf0b777d13923e9529967c6eaa5b5
hosinfefer2002 <hosinfefer2003@gmail.com>
2016-08-24 12:04:37 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,9 +42,9 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  exec { 'cowsay':
+  exec { "cowsay 'Welcome to ${::fqdn}!'":
   creates => '/etc/motd',
-  command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd'
+  path => '/usr/local/bin'
 }
   notify { "Greetings, this server's name is ${::hostname}": }
   include examples::fundamentals

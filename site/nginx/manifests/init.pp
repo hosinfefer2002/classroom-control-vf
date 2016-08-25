e1c6ba2eb23d58ea90218302f5cd4dd44fd3e7f1
hosinfefer2002 <hosinfefer2003@gmail.com>
2016-08-25 10:44:11 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -18,7 +18,7 @@ class nginx {
     mode => '0664',
     source => 'puppet:///modules/nginx/index.html'
 
-  file { '/etc/nginx/nginx.conf':
+  file {'/etc/nginx/nginx.conf':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
@@ -28,14 +28,14 @@ class nginx {
     notify => Service['nginx']
   }
 
-  file ['/etc/nginx/conf.d':
+  file {'/etc/nginx/conf.d':
     ensure => directory,
     owner => 'root',
     group => 'root',
     mode => '0755'
   }
 
-  file ['/etc/nginx/conf.d/default.conf':
+  file {'/etc/nginx/conf.d/default.conf':
     ensure => file,
     owner => 'root',
     group => 'root',

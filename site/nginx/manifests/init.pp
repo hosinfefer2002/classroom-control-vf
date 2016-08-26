5f70aca6f5bb6803891a080598e2c37def33e449
hosinfefer2002 <hosinfefer2003@gmail.com>
2016-08-26 13:29:49 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,39 +1,6 @@
 class nginx (
 $root = undef,
 ) {
-  case $::osfamily {
-    'Redhat':{
-      $package    = 'nginx'
-      $owner      = 'root'
-      $group      = 'root'
-      #$nginx_root = '/var/www'
-      $def_docroot = '/var/www'
-      $conf_dir   = '/etc/nginx'
-      $sblock_dir = '/etc/nginx/conf.d'
-      $log_dir    = '/var/log/nginx'
-      $user       = 'nginx'
-    }
-    'Debian':{
-      $package    = 'nginx'
-      $owner      = 'root'
-      $group      = 'root'
-      $nginx_root = '/var/www'
-      $conf_dir   = '/etc/nginx'
-      $sblock_dir = '/etc/nginx/conf.d'
-      $log_dir    = '/var/log/nginx'
-      $user       = 'www-data'
-    }
-    'Windows':{
-      $package    = 'nginx-service'
-      $owner      = 'Administrator'
-      $group      = 'Administrators'
-      $nginx_root = 'C:/ProgramData/nginx/html'
-      $conf_dir   = 'C:/ProgramData/nginx'
-      $sblock_dir = 'C:/ProgramData/nginx/conf.d'
-      $log_dir    = 'C:/ProgramData/nginx/logs'
-      $user       = 'nobody'
-    }
-  }
   
   $docroot = $root ? {
     default => $root,

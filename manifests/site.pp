766934322dff85b09e607d5cb914bdffd3923d78
hosinfefer2002 <hosinfefer2003@gmail.com>
2016-08-26 13:21:06 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,21 +1,4 @@
 ## site.pp ##
-
-# This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
-# used when an agent connects to a master and asks for an updated configuration.
-#
-# Global objects like filebuckets and resource defaults should go in this file,
-# as should the default node definition. (The default node can be omitted
-# if you use the console and don't define any other nodes in site.pp. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.)
-
-## Active Configurations ##
-
-# PRIMARY FILEBUCKET
-# This configures puppet agent and puppet inspect to back up file contents when
-# they run. The Puppet Enterprise console needs this to display file contents
-# and differences.
-
 # Disable filebucket by default for all File resources:
 File { backup => false }
 
@@ -43,7 +26,7 @@ node default {
   # Example:
   #   class { 'my_class': }
   class {'nginx':
-    root => '/var/www/html'
+    root => '/var/www/html1'
   }
   
   notify { hiera('message'): }

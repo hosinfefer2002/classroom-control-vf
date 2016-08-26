225071b66af60c582ab53fe0f236cd35f0de15a2
hosinfefer2002 <hosinfefer2003@gmail.com>
2016-08-26 11:03:22 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,46 +1,46 @@
 class nginx {
   case $::osfamily {
     'Redhat':{
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
+      $package    = 'nginx'
+      $owner      = 'root'
+      $group      = 'root'
       $nginx_root = '/var/www'
-      $conf_dir = '/etc/nginx'
+      $conf_dir   = '/etc/nginx'
       $sblock_dir = '/etc/nginx/conf.d'
-      $log_dir = '/var/log/nginx'
-      $user = 'nginx'
+      $log_dir    = '/var/log/nginx'
+      $user       = 'nginx'
     }
     'Debian':{
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
+      $package    = 'nginx'
+      $owner      = 'root'
+      $group      = 'root'
       $nginx_root = '/var/www'
-      $conf_dir = '/etc/nginx'
+      $conf_dir   = '/etc/nginx'
       $sblock_dir = '/etc/nginx/conf.d'
-      $log_dir = '/var/log/nginx'
-      $user = 'www-data'
+      $log_dir    = '/var/log/nginx'
+      $user       = 'www-data'
     }
     'Windows':{
-      $package = 'nginx-service'
-      $owner = 'Administrator'
-      $group = 'Administrators'
+      $package    = 'nginx-service'
+      $owner      = 'Administrator'
+      $group      = 'Administrators'
       $nginx_root = 'C:/ProgramData/nginx/html'
-      $conf_dir = 'C:/ProgramData/nginx'
+      $conf_dir   = 'C:/ProgramData/nginx'
       $sblock_dir = 'C:/ProgramData/nginx/conf.d'
-      $log_dir = 'C:/ProgramData/nginx/logs'
-      $user = 'nobody'
+      $log_dir    = 'C:/ProgramData/nginx/logs'
+      $user       = 'nobody'
     }
   }
   
   service { 'nginx':
-    ensure    => running,
-    enable    => true,
+    ensure => running,
+    enable => true,
   }
   
   File {
     owner => $owner,
     group => $group,
-    mode => '0664'
+    mode  => '0664'
   }
   
   package { $package:
@@ -52,15 +52,15 @@ class nginx {
   }
   
   file { "${conf_dir}/nginx.conf" :
-    ensure => file,
+    ensure  => file,
     content => template('nginx/nginx.conf.erb'),
-    notify => Service['nginx']
+    notify  => Service['nginx']
   }
   
   file { "${sblock_dir}/default.conf" :
-    ensure => file,
+    ensure  => file,
     content => template ('nginx/default.conf.erb'),
-    notify => Service['nginx']
+    notify  => Service['nginx']
   }
   
   file { "${nginx_root}/index.html" :
